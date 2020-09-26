61a66515944a005984f806789f5294b4156f9c35
KillTheBat-hub <54636019+KillTheBat-hub@users.noreply.github.com>
2020-09-26 22:02:00 +0800

    Update 问题记录.py

--- Languages/Python/问题记录.py
+++ Languages/Python/问题记录.py
@@ -20,3 +20,15 @@ except:
 finally:
     print("what ever print finally")  
 #先print finally，然后报错：TypeError, num2 not defined
+
+#open file
+#open一个不存在的file，会在当前文件夹创造这个file，但格式是TextIOWrapper而不是BufferRandom,可以write、close但是无法read
+#？？？解决方法？
+# 删除file后，依然能够查看为file命名的变量名和格式
+'''
+>>> newfile=open("newfile.txt","w")
+>>> newfile.close()
+>>> os.remove("newfile.txt")
+>>> newfile
+   <_io.TextIOWrapper name='newfile.txt' mode='w' encoding='cp936'>
+'''
