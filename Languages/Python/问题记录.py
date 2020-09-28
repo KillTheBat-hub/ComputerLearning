f95c6482d350e632ed47e094b992f5ec636747f9
KillTheBat-hub <54636019+KillTheBat-hub@users.noreply.github.com>
2020-09-28 11:43:12 +0800

    Update 问题记录.py

--- Languages/Python/问题记录.py
+++ Languages/Python/问题记录.py
@@ -23,7 +23,6 @@ finally:
 
 #open file
 #open一个不存在的file，会在当前文件夹创造这个file，但格式是TextIOWrapper而不是BufferRandom,可以write、close但是无法read
-#？？？解决方法？
 # 删除file后，依然能够查看为file命名的变量名和格式
 '''
 >>> newfile=open("newfile.txt","w")
@@ -34,3 +33,9 @@ finally:
 '''
 #解决方法：file.close()，再次以"w+"或"r"模式打开，依然是TextIOWrapper但是可以file.read()
 #write模式下没有read权限
+
+
+#file.read()
+#file.read()只能用一次，因为相当于光标会停到文档最后
+#file.close()后再次open，file.read()依然为空。解决方法有：file.seek(0)回到开头，试过后无效
+#？？？？？？？？？？？
