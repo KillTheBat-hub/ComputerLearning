fc0942a49d617f55fc64ed9b60f04e252fbab458
KillTheBat-hub <54636019+KillTheBat-hub@users.noreply.github.com>
2020-11-17 17:10:09 +0800

    Update liaoxuefeng_py3.py

--- Languages/Python/liaoxuefeng_py3.py
+++ Languages/Python/liaoxuefeng_py3.py
@@ -1,5 +1,7 @@
-#利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+'''
+利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
 #输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
+'''
 lis=['adam', 'LISA', 'barT']
 #1 recursion ????????
 def std(lis):
@@ -15,3 +17,6 @@ std_lis=map(cap,lis)
 #3 anonymous function 
 std_lis=map(lambda x:x.capitalize(),lis)
 print(list(std_lis))
+
+
+  
