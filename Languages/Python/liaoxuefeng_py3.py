db5e2a00d4bb043980d3e5d789f099723b37425f
KillTheBat-hub <54636019+KillTheBat-hub@users.noreply.github.com>
2020-11-17 17:30:36 +0800

    Update liaoxuefeng_py3.py

--- Languages/Python/liaoxuefeng_py3.py
+++ Languages/Python/liaoxuefeng_py3.py
@@ -2,21 +2,23 @@
 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
 #输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
 '''
-lis=['adam', 'LISA', 'barT']
+lis1=['adam', 'LISA', 'barT']
 #1 recursion ????????
 def std(lis):
-  lis=['adam', 'LISA', 'barT']
   for i in range(len(lis)):
     lis[i]=lis[i].capitalize()
+  #print(lis)
   return lis 
+print(upper(lis1))  
 #2 define function
 def cap(x):
   x=x.capitalize()
   return x
-std_lis=map(cap,lis)
+std_lis=map(cap,lis1)
 #3 anonymous function 
-std_lis=map(lambda x:x.capitalize(),lis)
-print(list(std_lis))
+std_lis=map(lambda x:x.capitalize(),lis1)
+print(list(std_lis1))   #注意std_lis1的type是map object，打印要先转化成list或者其他能直接打印的类型
+
 
 
   
